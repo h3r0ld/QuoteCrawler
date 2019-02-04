c35d3d6cc8ae9dd0139c0162ad1919e7eb8f0460
Kristof Herold <kristof.herold@gmail.com>
2019-02-04 19:54:16 +0100

    Initial commit

---
+++ QuoteCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuoteCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
